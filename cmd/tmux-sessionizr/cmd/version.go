04eb74e1b157e4a93357caccebc9963c840d98fd
Vojtěch Bargl <bargl.vojtech@gmail.com>
2025-01-13 23:40:38 +0100

    goreleaser update

--- cmd/tmux-sessionizr/cmd/version.go
+++ cmd/tmux-sessionizr/cmd/version.go
@@ -6,13 +6,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "dev"
+var Version = "dev"
 
 var versionCmd = cobra.Command{
 	Use:  "version",
 	Args: cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version)
+		fmt.Println(Version)
 	},
 }
 
